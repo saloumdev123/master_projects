a43dd5bf2f27427c53c6253f402c7482644cd646
modou <modou.gueye@gsietechnology.com>
2025-05-29 11:08:48 +0000

    last push for home screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,17 @@
+// src/app/app.component.ts
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { NavComponent } from "./components/nav/nav.component";
-import { FooterComponent } from "./components/footer/footer.component";
-import { JobComponent } from './components/job/job.component';
-import { AboutComponent } from "./components/about/about.component";
-import { ContactComponent } from "./components/contact/contact.component";
-import { PartnerComponent } from './components/partner/partner.component';
-import { HeroComponent } from './components/hero/hero.component';
-import { RecentJobComponent } from './components/recent-job/recent-job.component';
-import { CategoryComponent } from './components/category/category.component';
-import { CareerInfoComponent } from './components/career-info/career-info.component';
-import { TestimonialsComponent } from './components/testimonials/testimonials.component';
-import { BlogComponent } from './components/blog/blog.component';
-
+import { RouterOutlet } from '@angular/router'; 
+import { FooterComponent } from './components/footer/footer.component'; 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PartnerComponent, NavComponent, AboutComponent, ContactComponent, FooterComponent,JobComponent, HeroComponent, RecentJobComponent, CategoryComponent, CareerInfoComponent, TestimonialsComponent, BlogComponent],
+  imports: [
+    RouterOutlet, 
+    FooterComponent 
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'job-connect-app';
+  title = 'JobConnect';
 }
